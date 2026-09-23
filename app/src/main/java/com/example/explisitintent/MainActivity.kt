0b6af21d23299515e582ef9d9941e85fbf2959af
Baksoman <bryanraharjou@gmail.com>
2026-09-23 20:35:19 +0700

    feat: add topic 3

--- app/src/main/java/com/example/explisitintent/MainActivity.kt
+++ app/src/main/java/com/example/explisitintent/MainActivity.kt
@@ -57,6 +57,23 @@ class MainActivity : AppCompatActivity() {
             )
             resultLauncher.launch(intent)
         }
+
+        val isiPegawai = Pegawai(
+            1,
+            "Solman",
+            "Data Analyst"
+        )
+
+        val _btnExplisit3 = findViewById<Button>(R.id.btnExplisit3)
+        _btnExplisit3.setOnClickListener {
+            val intentWithData = Intent(
+                this@MainActivity,
+                MainActivity4::class.java
+            ).apply {
+                putExtra(MainActivity4.dataPegawai, isiPegawai)
+            }
+            startActivity(intentWithData)
+        }
     }
 
     private lateinit var _returnHasil: TextView
--- app/src/main/java/com/example/explisitintent/class.kt
+++
@@ -1,4 +0,0 @@
-package com.example.explisitintent
-
-class `class` {
-}
